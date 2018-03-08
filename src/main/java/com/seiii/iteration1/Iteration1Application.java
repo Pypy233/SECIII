1b36708dbd460ca1b01ecf05555fc9ed58846251
py-task <2529716798@qq.com>
2018-03-08 20:27:12 +0800

    ...

---
+++ src/main/java/com/seiii/iteration1/Iteration1Application.java
@@ -0,0 +1,12 @@
+package com.seiii.iteration1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iteration1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Iteration1Application.class, args);
+	}
+}
